d061c06f918f8084dbdf993bfcbedf618177a0d7
Divyanshu Sharma <divyanshu.sharma@truringsoftek.com>
2018-09-21 11:07:55 +0530

    globalization

--- node_service/complete.js
+++ node_service/complete.js
@@ -2,40 +2,42 @@ const axios = require('axios');
 const fs = require('fs');
 
 var filelocation;
+let unique = []
 
 axios.get('http://localhost:3000/filepath')
     .then(function (response) {
-    // handle success
-    console.log(response.data);
-    filelocation = response.data;
-    let rawData = fs.readFileSync(filelocation.toString());
-    let objectDetail = JSON.parse(rawData);
-    let keys = Object.keys(objectDetail).toString();
-    let values = objectDetail[keys];
-    let unique = []
-    let all = []
-    for(let i=0; i<values.length; i++) {
-        
-        if (Object.keys(values[i]).length > 1) {
-            for (let j = 0; j < Object.keys(values[i]).length; j++) {
-                all.push(Object.keys(values[i])[j]);
+        filelocation = response.data;
+        let rawData = fs.readFileSync(filelocation.toString());
+        let objectDetail = JSON.parse(rawData);
+        let keys = Object.keys(objectDetail).toString();
+        let values = objectDetail[keys];
+        let all = []
+        for(let i=0; i<values.length; i++) {
+            
+            if (Object.keys(values[i]).length > 1) {
+                for (let j = 0; j < Object.keys(values[i]).length; j++) {
+                    all.push(Object.keys(values[i])[j]);
+                }
+            }
+            else {
+                all.push(Object.keys(values[i]).toString())
             }
         }
-        else {
-            all.push(Object.keys(values[i]).toString())
+
+        for(i = 0; i<all.length; i++) {
+            if (!unique.includes(all[i])) { unique.push(all[i])}
         }
-    }
-
-    for(i = 0; i<all.length; i++) {
-        if (!unique.includes(all[i])) { unique.push(all[i])}
-    }
-
-    console.log(all, " ------YOLO-------- ", unique);
+        console.log(unique);
+        
+        axios.post()
     })
+
     .catch(function (error) {
     // handle error
     console.log(error);
     })
+
     .then(function () {
     // always executed
     });
+
